bed951f89c022b02aad2296e8d6114e49d37f6e6
danielch1 <d.chroscicki@gmail.com>
2023-09-28 10:05:12 +0200

    [ADD] Repo structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLOps project as a part of Master studies at University of Munich, summer semester 2023',
+    author='positive_team',
+    license='MIT',
+)
