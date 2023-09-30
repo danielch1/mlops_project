6414dc397fd84b553c318823ae0e1114ffc8aa75
LennMarx <131344012+LennMarx@users.noreply.github.com>
2023-09-30 12:42:49 +0200

    implementation of first unit test, test on unix based terminal necessary, anaconda terminal not working properly works within the script

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,19 @@
-def test_data():
-    dataset = MNIST(...)
-    assert len(dataset) == N_train for training and N_test for test
-    assert that each datapoint has shape [1,224,224]
-    assert that all labels are represented
+from tests import _PATH_DATA
+import torch
+import os
+from src.data.make_dataset import Lego_Dataset
+import pytest
+
+@pytest.mark.parametrize('data_path,expected_data_shape', [
+    (os.path.join(_PATH_DATA,'processed','train_dataset.pth'), torch.Size([3, 224, 224])),
+    (os.path.join(_PATH_DATA,'processed','val_dataset.pth'), torch.Size([3, 224, 224])),
+    (os.path.join(_PATH_DATA,'processed','test_dataset.pth'), torch.Size([3, 224, 224]))
+])
+
+
+def test_data_shape(data_path,expected_data_shape):
+    dataset = torch.load(data_path)
+    img,_ = dataset.__getitem__(0)
+    assert img.shape == expected_data_shape
+
+
