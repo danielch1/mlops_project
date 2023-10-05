c9290b9ada74887f8c6d3308daf563a4eeda7433
LennMarx <131344012+LennMarx@users.noreply.github.com>
2023-10-05 11:06:22 +0200

    added predict function to interact with a user, takes image as input and returns the prediction as a string

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,11 +1,14 @@
 import torch
 from torch.utils.data import DataLoader
-from src.data.make_dataset import make_dataset
+from src.data.make_dataset import make_dataset,get_transform,convert_label
 from src import _PROJECT_ROOT
 import os
 from timm import create_model
 import hydra
 import numpy as np
+from omegaconf import DictConfig
+from PIL import Image
+
 
 
 def load_model(model_path):
@@ -15,7 +18,7 @@ def load_model(model_path):
     return model
 
 
-def predict(config):
+def predict_test(config):
     dataloader = DataLoader(
         make_dataset(config, dataset_type="test"), batch_size=32, shuffle=False
     )
@@ -33,9 +36,30 @@ def predict(config):
 
     return predictions
 
+def predict(config : DictConfig,img : Image) -> str:
+
+    model = load_model(
+        model_path=os.path.join(_PROJECT_ROOT, "models", "mobilenetv3_fine_tuned.pth")
+    )
+
+    config
+
+    with torch.no_grad():
+        transforms = get_transform(config, dataset_type= "test")
+        image_rgb = img.convert("RGB")
+        processed = transforms(image_rgb).unsqueeze(0)
+        outputs = model(processed)
+        class_id = torch.argmax(outputs, dim=1).cpu().detach().numpy()[0]
+    prediction = convert_label(class_id)
+    return prediction
+
+
 
 @hydra.main(config_path="../../config/", config_name="main.yaml")
 def main(cfg):
-    predictions = predict(cfg)
-    save_path = os.path.join(_PROJECT_ROOT, "src", "models", "predictions.csv")
-    np.savetxt(save_path, predictions, delimiter=",")
+    prediction = predict(cfg,Image.open('C:/Users/Lennart/Documents/GitHub/mlops_project/data/external/lego_dataset/test/005.jpg'))
+    print(prediction)
+
+
+
+main()
