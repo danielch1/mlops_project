c39867f84120ead9ac046575cc6df27703156b9e
danielch1 <d.chroscicki@gmail.com>
2023-10-06 00:00:11 +0200

    [Chng] Flake, pre-commit change and formatting

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='MLOps project as a part of Master studies at University of Munich, summer semester 2023',
-    author='positive_team',
-    license='MIT',
+    version="0.1.0",
+    description=(
+        "MLOps project as a part of Master studies at University of Munich, "
+        "summer semester 2023"
+    ),
+    author="positive_team",
+    license="MIT",
 )
