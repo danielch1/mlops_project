074b63ad636c839ff2f94f8426d30008c411139e
LennMarx <131344012+LennMarx@users.noreply.github.com>
2023-10-10 14:56:10 +0200

    more formatting

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -9,36 +9,26 @@ from hydra import compose, initialize
 from PIL import Image
 from pydantic import BaseModel
 from timm import create_model
-from torch.utils.data import DataLoader
 
-from src import _PROJECT_ROOT
-from src.data.make_dataset import convert_label, get_transform, make_dataset
+from src.data.make_dataset import convert_label, get_transform
 
+# def predict_test(config):
+#     dataloader = DataLoader(
+#         make_dataset(config, dataset_type="test"), batch_size=32, shuffle=False
+#     )
+#     predictions = []
 
-def load_model(model_path):
-    model = create_model("mobilenetv3_large_100", pretrained=False, num_classes=38)
-    model.load_state_dict(torch.load(model_path))
-    model.eval()  # Set the model to evaluation mode
-    return model
+#     model = load_model(
+#         model_path=os.path.join(_PROJECT_ROOT, "models", "mobilenetv3_fine_tuned.pth")
+#     )
 
+#     with torch.no_grad():
+#         for batch_idx, (batch, _) in enumerate(dataloader):
+#             inputs = batch
+#             outputs = model(inputs)
+#             predictions.extend(torch.argmax(outputs, dim=1).cpu().detach().numpy())
 
-def predict_test(config):
-    dataloader = DataLoader(
-        make_dataset(config, dataset_type="test"), batch_size=32, shuffle=False
-    )
-    predictions = []
-
-    model = load_model(
-        model_path=os.path.join(_PROJECT_ROOT, "models", "mobilenetv3_fine_tuned.pth")
-    )
-
-    with torch.no_grad():
-        for batch_idx, (batch, _) in enumerate(dataloader):
-            inputs = batch
-            outputs = model(inputs)
-            predictions.extend(torch.argmax(outputs, dim=1).cpu().detach().numpy())
-
-    return predictions
+#     return predictions
 
 
 client = storage.Client()
--- tests/test_model.py
+++ tests/test_model.py
@@ -2,8 +2,8 @@ import os
 
 import pytest
 import torch
+from timm import create_model
 
-from src.models.predict_model import load_model
 from tests import _PROJECT_ROOT
 
 # Define the path to the file you want to check for existence
@@ -13,7 +13,8 @@ file_to_check = os.path.join(_PROJECT_ROOT, "models", "mobilenetv3_fine_tuned.pt
 # Use the pytest.mark.skipif decorator to conditionally skip the test
 @pytest.mark.skipif(not os.path.exists(file_to_check), reason="File not found")
 def test_output_shape():
-    model = load_model(file_to_check)
+    model = create_model("mobilenetv3_large_100", pretrained=False, num_classes=38)
+    model.load_state_dict(torch.load(file_to_check))
     model.eval()
     output = model(torch.zeros(1, 3, 224, 224))
     assert output.shape == torch.Size(
